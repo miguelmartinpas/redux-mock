c25d7827464fabb0be0072dbb7bef40c789d2a59
Miguel Martin <miguel.martin@adsmurai.com>
2022-05-07 18:46:27 +0200

    create base skeleton v2

--- src/organisms/TodoForm/TodoForm.tsx
+++ src/organisms/TodoForm/TodoForm.tsx
@@ -1,33 +1,44 @@
 import React, { useState } from 'react'
 import {
-    Checkbox,
     Form,
+    FormInputProps,
     FormSelectProps,
     Input,
-    Radio,
     Select,
-    TextArea,
   } from 'semantic-ui-react'
 
 const TodoForm = (): React.ReactElement => {
     const [filter, setFilter] = useState<string>('all');
+    const [task, setTask] = useState<string>('');
+
+    const handleOnSubmit = (): void => {
+        console.log('Add new task', task);
+    }
+
+    const handleFilter = (filter: string): void => {
+        console.log('Apply filter', filter);
+        setFilter(filter)
+    }
 
     return (
-        <Form>
-            <Form.Group widths='equal'>
-            <Form.Field
-                control={Input}
-                label='Task'
-                placeholder=''
-            />
-            <Form.Field
-                control={Select}
-                value={filter}
-                onChange={(event: React.MouseEvent<HTMLButtonElement>, item: FormSelectProps) => setFilter(item.value as string)}
-                label='Task filter'
-                options={[{key: 'a', value: 'all', text: 'All'}, {key: 'd', value: 'done', text: 'Done'}, {key: 'p', value: 'pending', text: "Pending"}]}
-                placeholder='Select one'
-            />
+        <Form onSubmit={handleOnSubmit}>
+            <Form.Group widths={2}>
+                <Form.Field
+                    width={12}
+                    control={Input}
+                    label=''
+                    placeholder=''
+                    onChange={(event: React.MouseEvent<HTMLButtonElement>, data: FormInputProps) => setTask(data.value as string)}
+                />
+                <Form.Field
+                    width={4}
+                    control={Select}
+                    value={filter}
+                    onChange={(event: React.MouseEvent<HTMLButtonElement>, data: FormSelectProps) => handleFilter(data.value as string)}
+                    label=''
+                    options={[{key: 'a', value: 'all', text: 'All'}, {key: 'd', value: 'done', text: 'Done'}, {key: 'p', value: 'pending', text: "Pending"}]}
+                    placeholder='Select one'
+                />
             </Form.Group>
         </Form>
     );
